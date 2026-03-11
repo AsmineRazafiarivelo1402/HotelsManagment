49107334f8526da30807b4e004563113760c241b
Miriasme <johndoe@example.com>
2026-03-11 15:29:00 +0300

    feat: Q5c

--- src/main/java/org/hei/hotelmanagment/ReservationController.java
+++ src/main/java/org/hei/hotelmanagment/ReservationController.java
@@ -1,5 +1,7 @@
 package org.hei.hotelmanagment;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,4 +39,36 @@ public class ReservationController {
 
         return ReservationList.reservationLists;
     }
+    @PostMapping("/booking/check")
+    public List<ReservationModel> createReservationWithChecking(
+            @RequestParam String customer_name,
+            @RequestParam String customer_phone_number,
+            @RequestParam String customer_mail_adress,
+            @RequestParam int room_number,
+            @RequestParam String room_description,
+            @RequestParam LocalDateTime reservation_date
+    ) {
+        for (ReservationModel existingReservation : ReservationList.reservationLists) {
+            if (existingReservation.getRoom_number() == room_number &&
+                    existingReservation.getReservation_date().equals(reservation_date)) {
+
+                System.out.println("La chambre " + room_number + " est déjà réservée pour la date " + reservation_date);
+                return ReservationList.reservationLists;
+            }
+        }
+
+        ReservationModel newReservation = new ReservationModel(
+                customer_mail_adress,
+                customer_name,
+                customer_phone_number,
+                reservation_date,
+                room_description,
+                room_number
+        );
+
+        ReservationList.reservationLists.add(newReservation);
+
+        return ReservationList.reservationLists;
+    }
+
 }
