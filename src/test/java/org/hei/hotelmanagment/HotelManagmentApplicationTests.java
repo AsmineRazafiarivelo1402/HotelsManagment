23ed3ee6b3aec21e0e48f6b490bb38f403c93098
Miriasme <johndoe@example.com>
2026-03-11 14:03:53 +0300

    create project for hotel managmennt

---
+++ src/test/java/org/hei/hotelmanagment/HotelManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hei.hotelmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
