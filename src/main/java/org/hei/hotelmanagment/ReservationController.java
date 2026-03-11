41bfbbce8067c1dc961d4ae0bb8476a45bb28122
Miriasme <johndoe@example.com>
2026-03-11 15:15:44 +0300

    feat: Q5b

--- src/main/java/org/hei/hotelmanagment/ReservationController.java
+++ src/main/java/org/hei/hotelmanagment/ReservationController.java
@@ -2,8 +2,10 @@ package org.hei.hotelmanagment;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -13,7 +15,26 @@ public class ReservationController {
         return ReservationList.reservationLists;
     }
     @PostMapping("/booking")
-    public String createReservation(){
-        return "Reservation created successfully";
+    public List<ReservationModel> createReservation(
+            @RequestParam String customer_name,
+            @RequestParam String customer_phone_number,
+            @RequestParam String customer_mail_adress,
+            @RequestParam int room_number,
+            @RequestParam String room_description,
+            @RequestParam LocalDateTime reservation_date
+    ) {
+
+        ReservationModel newReservation = new ReservationModel(
+                customer_mail_adress,
+                customer_name,
+                customer_phone_number,
+                reservation_date,
+                room_description,
+                room_number
+        );
+
+        ReservationList.reservationLists.add(newReservation);
+
+        return ReservationList.reservationLists;
     }
 }
